ba46ed303247ca88c153b6d55c86edab45f9211b
Jorge De la Vega <jorgedelavg04@gmail.com>
2020-09-12 21:06:34 -0500

    added html injection

--- Chat_ReactJS/src/App.js
+++ Chat_ReactJS/src/App.js
@@ -1,59 +1,78 @@
 //React Components
-import React from "react";
+import React, { Component } from "react";
 import NavBar from "./Components/NavBar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Widget, addResponseMessage } from "react-chat-widget";
+import { Widget, addResponseMessage, renderCustomComponent } from "react-chat-widget";
 import "react-chat-widget/lib/styles.css";
 import Home from "./Pages/Home";
 import LandingPage from "./Pages/LandingPage";
 import User from "./Pages/User";
 import ReactHtmlParser from 'html-react-parser';
 import axios from 'axios';
+import '../src/App.css'
 
-const handleNewUserMessage = (newMessage) => { 
-  sendData(newMessage).then(function(response) {
-    addResponseMessage(String(response[0]));
-  });
-};
-
-const url = 'http://127.0.0.1:5002/getMessage'
-async function sendData(messageFromUser) {
-
-  return axios.post(url, {
-    message: messageFromUser
-  })
-  .then((response) => {
-    const watson_response = response.data.response_watson.watson_answer;
-    const watson_intent = response.data.response_watson.watson_intent;
-    var array_response = []
-    array_response = [watson_response, watson_intent]
-    return array_response;
-  })
-  .catch(function (error) {
-      console.log("Error: " + error );
-  });
+//This is the image component, copy it to your project
+class Text123 extends Component {
+  render() {
+  return <div className="speech-bubble">{ReactHtmlParser(this.props.text)}</div>
+  }
 }
 
-export default function App() {
-  return (
-    <Router>
-      <NavBar />
-      <Widget
-        handleNewUserMessage={handleNewUserMessage}
-        title="Laboratorio Web"
-        subtitle="Bienvenido"
-      />
-      <Switch>
-        <Route exact path="/">
-          <LandingPage />
-        </Route>
-        <Route path="/home">
-          <Home />
-        </Route>
-        <Route path="/user">
-          <User />
-        </Route>
-      </Switch>
-    </Router>
-  );
+class App extends Component {
+  /*
+  handleNewUserMessage = (newMessage) => { 
+    this.sendData(newMessage).then(function(response) {
+      addResponseMessage(String(response[0]));
+    });
+
+  };*/
+
+  handleNewUserMessage = (newMessage) => {
+    this.sendData(newMessage)
+  }
+
+
+  async sendData(messageFromUser) {
+    return axios.post('http://127.0.0.1:5002/getMessage', {
+      message: messageFromUser
+    })
+    .then((response) => {
+      const watson_response = response.data.response_watson.watson_answer;
+      const watson_intent = response.data.response_watson.watson_intent;
+      var array_response = []
+      array_response = [watson_response, watson_intent]
+      //Aquí es donde se envia el texto con el renderCustomComponent
+      renderCustomComponent(Text123, {text: watson_response})
+      return array_response;
+    })
+    .catch(function (error) {
+        console.log("Error: " + error );
+    });
+  }
+
+  render() {
+    return (
+      <Router>
+        <NavBar />
+        <Widget
+          handleNewUserMessage={this.handleNewUserMessage}
+          title="Laboratorio Web"
+          subtitle="Bienvenido"
+        />
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/user">
+            <User />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
+
+export default App;
