8736c76d6fa270cbe84f64eecef86f4a9f7c72c6
Jorge De la Vega <jorgedelavg04@gmail.com>
2020-09-14 09:43:03 -0500

    First conversation path added

--- Chat_ReactJS/src/App.js
+++ Chat_ReactJS/src/App.js
@@ -2,19 +2,27 @@
 import React, { Component } from "react";
 import NavBar from "./Components/NavBar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Widget, addResponseMessage, renderCustomComponent } from "react-chat-widget";
+import { Widget, addResponseMessage, renderCustomComponent, setQuickButtons, addUserMessage, toggleMsgLoader} from "react-chat-widget";
 import "react-chat-widget/lib/styles.css";
 import Home from "./Pages/Home";
 import LandingPage from "./Pages/LandingPage";
 import User from "./Pages/User";
 import ReactHtmlParser from 'html-react-parser';
 import axios from 'axios';
-import '../src/App.css'
+import '../src/App.css';
+import good_job from '../src/Images/well_done.gif';
+import chatbot from '../src/Images/chatbot.jpg'
 
 //This is the image component, copy it to your project
 class Text123 extends Component {
   render() {
   return <div className="speech-bubble">{ReactHtmlParser(this.props.text)}</div>
+  }
+}
+
+class Image extends Component {
+  render() {
+    return <img src={this.props.src}></img>
   }
 }
 
@@ -28,8 +36,16 @@ class App extends Component {
   };*/
 
   handleNewUserMessage = (newMessage) => {
+    toggleMsgLoader();
     this.sendData(newMessage)
+
   }
+
+  handleQuickButtonClicked = (value) => {
+    addUserMessage(value)
+    this.handleNewUserMessage(value)
+    
+  };
 
 
   async sendData(messageFromUser) {
@@ -37,12 +53,27 @@ class App extends Component {
       message: messageFromUser
     })
     .then((response) => {
+      console.log(response)
       const watson_response = response.data.response_watson.watson_answer;
       const watson_intent = response.data.response_watson.watson_intent;
       var array_response = []
       array_response = [watson_response, watson_intent]
-      //Aquí es donde se envia el texto con el renderCustomComponent
-      renderCustomComponent(Text123, {text: watson_response})
+      console.log(watson_intent)
+      console.log(watson_response)
+      
+      toggleMsgLoader();
+      if (watson_intent === 'Saludo') {
+        renderCustomComponent(Image, {src: chatbot}) 
+        renderCustomComponent(Text123, {text: watson_response});
+        setQuickButtons([ { label: 'Nombre de la clase', value: 'Nombre de la clase' }, { label: 'Nombre del profesor', value: 'Nombre del profesor'} ]);
+
+      } else if (watson_intent === 'Agradecimiento') {
+        renderCustomComponent(Image, {src: good_job}) 
+        renderCustomComponent(Text123, {text: watson_response})
+      } else {
+        setQuickButtons([])
+        renderCustomComponent(Text123, {text: watson_response})
+      }
       return array_response;
     })
     .catch(function (error) {
@@ -56,6 +87,7 @@ class App extends Component {
         <NavBar />
         <Widget
           handleNewUserMessage={this.handleNewUserMessage}
+          handleQuickButtonClicked={this.handleQuickButtonClicked}
           title="Laboratorio Web"
           subtitle="Bienvenido"
         />
