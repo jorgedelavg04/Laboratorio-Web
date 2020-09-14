7edafcf7a41a99f2c0b00a2d3a1cce79a75db637
Jorge De la Vega <jorgedelavg04@gmail.com>
2020-09-14 13:15:05 -0500

    added changes to chatbot

--- Chat_ReactJS/src/App.js
+++ Chat_ReactJS/src/App.js
@@ -37,8 +37,11 @@ class App extends Component {
 
   handleNewUserMessage = (newMessage) => {
     toggleMsgLoader();
-    this.sendData(newMessage)
-
+    if (newMessage == "RESET") {
+      this.sendData("Hola")
+    }else {
+      this.sendData(newMessage)
+    }
   }
 
   handleQuickButtonClicked = (value) => {
@@ -56,21 +59,37 @@ class App extends Component {
       console.log(response)
       const watson_response = response.data.response_watson.watson_answer;
       const watson_intent = response.data.response_watson.watson_intent;
+      const watson_context_nombre = response.data.response_watson.watson_context_nombre;
       var array_response = []
-      array_response = [watson_response, watson_intent]
+      array_response = [watson_response, watson_intent, watson_context_nombre]
       console.log(watson_intent)
       console.log(watson_response)
+      console.log(watson_context_nombre)
       
       toggleMsgLoader();
       if (watson_intent === 'Saludo') {
         renderCustomComponent(Image, {src: chatbot}) 
         renderCustomComponent(Text123, {text: watson_response});
-        setQuickButtons([ { label: 'Nombre de la clase', value: 'Nombre de la clase' }, { label: 'Nombre del profesor', value: 'Nombre del profesor'} ]);
-
+        renderCustomComponent(Text123, {text: "<p>¿Cual es tu nombre?</p>"});
+        
       } else if (watson_intent === 'Agradecimiento') {
-        renderCustomComponent(Image, {src: good_job}) 
-        renderCustomComponent(Text123, {text: watson_response})
-      } else {
+        if (watson_response === "<p>Gracias, vuelve pronto 🙌.</p>") {
+          renderCustomComponent(Text123, {text: watson_response})
+          setQuickButtons([])
+        } else if (watson_response === "<p>Por favor selecciona una de las opciones o escribe lo que necesites</p>") {
+          renderCustomComponent(Text123, {text: watson_response})
+          setQuickButtons([ { label: 'Nombre de la clase', value: 'Nombre de la clase' }, { label: 'Nombre del profesor', value: 'Nombre del profesor'} ]);
+        } else {
+          setQuickButtons([ { label: 'Si 👍', value: 'Si 👍' }, { label: 'No 👋', value: 'No 👋'} ]);
+          renderCustomComponent(Image, {src: good_job}) 
+          renderCustomComponent(Text123, {text: watson_response})
+        }
+      } else if (watson_response === "<p>"+watson_context_nombre+",  ¿qué puedo hacer por ti?</p>") {
+        renderCustomComponent(Text123, {text: watson_response});
+        setQuickButtons([ { label: 'Nombre de la clase', value: 'Nombre de la clase' }, { label: 'Nombre del profesor', value: 'Nombre del profesor'} ]);
+      }else {
+        console.log("aqui ando")
+        console.log(watson_response)
         setQuickButtons([])
         renderCustomComponent(Text123, {text: watson_response})
       }
